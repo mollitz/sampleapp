74a3f7430968169e1d9e6929b4be1dc0b33327de
Moritz Schäfer <mollitz@gmail.com>
2013-02-02 03:20:36 +0100

    lalala

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,15 @@ describe User do
   it{should respond_to(:remember_token) }
   it{should respond_to(:authenticate)}
   it{should be_valid}
+  it {should_not be_admin}
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    it {should be_admin}
+  end
 
   describe "return value of authenticate method" do
     before {@user.save}
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,6 +20,7 @@ describe "Authentication" do
       let(:user) {FactoryGirl.create(:user)}
       before {sign_in user}
       it {should have_selector('title', text: user.name) }
+      it {should have_link('Users', href: users_path) }
       it {should have_link('Profile', href: user_path(user))}
       it {should have_link('Settings', href: edit_user_path(user))}
       it {should have_link('Sign out', href: signout_path)}
@@ -53,6 +54,10 @@ describe "Authentication" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
+        describe "visiting the user index" do
+          before {visit users_path}
+          it {should have_selector('title', text:'Sign in')}
+        end
       end
     end
     describe "as wrong user" do
@@ -67,6 +72,15 @@ describe "Authentication" do
       describe "submitting a PUT request to the Users#update action" do
         before { put user_path(wrong_user)}
         specify { response.should redirect_to(root_path) } 
+      end
+    end
+    describe "as non-admin user" do
+      let(:user) {FactoryGirl.create(:user) }
+      let(:non_admin) {FactoryGirl.create(:user) }
+      before { sign_in non_admin }
+      describe "submitting DELETE request to a User" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
       end
     end
   end
